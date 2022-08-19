9be1a18ab2bc3ca9fc19ffc9b5ed4d462c2afe98
Mr Porchouayang <porchouayangdevelop@gmail.com>
2022-08-19 18:18:28 +0700

    fix

--- app.js
+++ app.js
@@ -44,9 +44,9 @@ app.use((req, res, next) => {
 
 // req set header
 app.use((req, res, next) => {
-    res.set('Access-Control-Allow-Origin', '*');
-    res.set('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-    res.set('Access-Control-Allow-Methods', 'GET, POST,PUT, PATCH, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    res.header('Access-Control-Allow-Methods', 'GET, POST,PUT, PATCH, DELETE, OPTIONS');
     next();
 });
 
